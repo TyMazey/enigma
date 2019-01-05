fbd2a2e1943855f6b05f5bd05b36b0130f6f6d53
Ty Mazey <tymazey@gmail.com>
2019-01-05 15:17:37 -0700

    adds working todays date class

--- test/todays_date_test.rb
+++ test/todays_date_test.rb
@@ -10,13 +10,20 @@ class TodaysDateTest < Minitest::Test
 
   def test_it_can_return_todays_date
     today = TodaysDate.new
-    exact = Date.new.today.strftime
+    exact = Date.today
 
-    assert_equal "2019-01-05", today.today
+    assert_equal exact.strftime, today.today
+  end
+
+  def test_it_can_return_current_year_as_yy_not_yyyy
+    today = TodaysDate.new
+
+    assert_equal "19", today.yy
   end
 
   def test_it_can_return_date_as_dd_mm_yy
     today = TodaysDate.new
+
 
     assert_equal "050119", today.short_hand
   end
