a2d905b929602fcc42fdd6693236c3bc7f50afc7
Ty Mazey <tymazey@gmail.com>
2019-01-06 11:46:55 -0700

    message shifter class now inherits from alphabet shifter

--- lib/message_shifter.rb
+++ lib/message_shifter.rb
@@ -1,27 +1,29 @@
-class MessageShifter
+require_relative 'alphabet_shifter'
+
+class MessageShifter < AlphabetShifter
   attr_reader :alphabet,
               :message
 
-  def initialize(message)
-    @alphabet = ('a'..'z').to_a.push(' ')
-    @message = message.chars
+  def initialize
+    super
   end
 
   def key_rotation(key_value)
     key_value.values
   end
 
-  def shift_message(key)
+  def shift_message(message, key)
     key_rotation = key_rotation(key)
-    @message.map do |letter|
-      new_letter = Hash[@alphabet.zip(@alphabet.rotate(key_rotation[0]))]
+    message.chars.map do |letter|
+      new_letter = shift(key_rotation[0])
       key_rotation.rotate!(1)
       new_letter[letter.downcase]
     end
   end
 
   def self.new_message(message, key)
-    new_mssg = new(message)
-    new_mssg.shift_message(key).join
+    new_mssg = new
+    new_mssg.alphabet(' ')
+    new_mssg.shift_message(message, key).join
   end
 end
